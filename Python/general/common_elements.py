f248debf3a889ab01f36f19589ed08325b4e72de
Corwin Brown <blakfeld@Nightwing.local>
2017-02-04 09:06:21 -0600

    Find Common Elements in Array

--- Python/general/common_elements.py
+++ Python/general/common_elements.py
@@ -8,6 +8,7 @@ Author: Corwin Brown <blakfeld@gmail.com>
 from __future__ import print_function
 
 import sys
+from collections import defaultdict
 
 
 def find_common_elements(list_a, list_b):
@@ -21,14 +22,21 @@ def find_common_elements(list_a, list_b):
     Returns:
         list
     """
-    pass
+    if len(list_a) > len(list_b):
+        larger, smaller = list_a, list_b
+    else:
+        larger, smaller = list_b, list_a
+
+    return [elem for elem in smaller if elem in set(larger)]
 
 
 def main():
     """
     Main.
     """
-    pass
+    list_a = [1, 2, 5, 9, 7, 0, 4]
+    list_b = [5, 3, 1, 6, 9, 8, 7, 3, 2]
+    print(find_common_elements(list_a, list_b))
 
 
 if __name__ == '__main__':
